cf017acc78eda2ecd602e0d0f6e38805ee77ffcb
Abdullah Nazim <abdullahnazim55@gmail.com>
2026-07-25 19:29:42 +0300

    add script to create new employee

---
+++ powershell/01-Create-NewEmployee.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+Creates a new employee account in Microsoft Entra ID.
+
+.DESCRIPTION
+This script automates the onboarding process for new employees by creating
+a Microsoft Entra ID user, populating user attributes, assigning a manager,
+adding the user to the appropriate security group, and generating a log.
+
+.AUTHOR
+Abdullah Nazim
+
+.PROJECT
+Enterprise Onboarding Automation
+Apex Innovations
+
+.VERSION
+1.0
+#>
